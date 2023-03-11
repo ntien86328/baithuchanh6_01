4cbf07998037a2154c42f6fc5972182a6f673529
ntien86328 <tienlinhnt20022gmail.com>
2023-03-11 16:55:57 +0700

    first commit

---
+++ cau2.dart
@@ -0,0 +1,25 @@
+class House{
+  var id;
+  String? name;
+  var prize;
+  House(var id, String name, var priz)
+  {
+      this.id = id;
+      this.name = name;
+      this.prize = priz;
+  }
+  void printInfo(){
+    print('ID: $id');
+    print('Name: $name');
+    print('Ram: $prize');
+    print('');
+  }
+}
+void main(){
+  House hous1 = new House('H1', 'House Name 1', 'Prize 1');
+  House hous2 = new House('H2', 'House Name 2', 'Prize 2');
+  House hous3 = new House('H3', 'House Name 3', 'Prize 3');
+  hous1.printInfo();
+  hous2.printInfo();
+  hous3.printInfo();
+}
